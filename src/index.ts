bf9e5c694ce7256af796123802b5353467fbbae6
yiiu <yu.gintoki@gmail.com>
2017-11-23 23:09:05 +0800

    add mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import * as express from 'express'
+import * as mongodb from 'mongodb'
 
 const app = express()
 
-app.set('port', process.env.PORT || 3000);
+const url = 'mongodb://soap:soap@localhost:27017/soap'
+
+app.set('port', process.env.PORT || 3000)
 
 app.get('/', (req, res) => {
   return res.json({
     data: 1
   })
-});
+})
 
-app.listen('2333', () => {
+mongodb.MongoClient.connect(url, () => {
+  console.log(123123)
+})
+
+app.listen(app.get('port'), () => {
   console.log(('  App is running at http://localhost:%d'), app.get('port'))
   console.log('  Press CTRL-C to stop\n')
-});
+})
